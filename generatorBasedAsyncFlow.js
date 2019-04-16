2895a1498e23cb4228ae88e207429ff9086eb5e5
jiangweixu <jiangweixu@tencent.com>
2019-04-16 17:27:56 +0800

    add asyncFlowWithThunks based on Thunk

--- generatorBasedAsyncFlow.js
+++ generatorBasedAsyncFlow.js
@@ -37,3 +37,27 @@ function asyncFlowWithThunks(generatorFunction) {
     const thunk = generator.next().value;
     thunk && thunk(callback);
 }
+
+/*
+*  A Thunk function is something likes this
+*  A thunk used in the generator-based control flow is just a function that
+    partially applies all the arguments of the original function except its
+    callback. The return value is another function that only accepts the
+    callback as an argument. For example, the thunkified version of
+    fs.readFile() would be as follows:
+* */
+function readFileThunk(filename, options) {
+    return function(callback){
+        fs.readFile(filename, options, callback);
+    }
+}
+
+/*
+* A asyc flow based on asyncFlowWithThunks demo.
+* */
+asyncFlowWithThunks(function* () {
+    const fileName = path.basename(__filename);
+    const myself = yield readFileThunk(__filename, 'utf8');
+    yield writeFileThunk(`clone_of_${fileName}`, myself);
+    console.log("Clone created");
+});
