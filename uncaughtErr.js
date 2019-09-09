6064d0e79b4cfb8263c6d0d78ea2abd2f4aca7a5
xujiangwei <xujiangwei@bytedance.com>
2019-09-09 21:20:35 +0800

    feat: add unCaught.js

---
+++ uncaughtErr.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+
+function readJSONThrows(filename, callback) {
+  fs.readFile(filename, 'utf8', (err, data) => {
+    if(err) {
+      return callback(err);
+    }
+    //no errors, propagate just the data
+    callback(null, JSON.parse(data));
+  });
+}
+
+try {
+  readJSONThrows('noExist.html', err => {
+    console.log(123);
+    console.log(err);
+  });
+} catch (err) {
+  console.log('try catch: ', err);
+}
+
+// 回答打印出来什么内容，为什么？
+
+// 涉及到Event Loop执行机制，简单
