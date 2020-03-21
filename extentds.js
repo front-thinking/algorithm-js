87e469cf5e5e0676c278b99262111116d423eac8
front-thinking <xjw919@qq.com>
2020-03-22 01:07:22 +0800

    Update extentds.js

--- extentds.js
+++ extentds.js
@@ -1,3 +1,6 @@
+// 两个关键点：1 原型链的处理， 2构造函数的指向
+// 第8行得到的结果是d.prototype = {__proto__ :__.prototype, d.constructor = d}，即改变了原型链，也保留了构造函数的指向
+
 var __extends = this.__extends || function (d, b) {
   for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; 
   function __() { this.constructor = d; }
